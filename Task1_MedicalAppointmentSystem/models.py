b49752f6849af9780baf93a25f67bc6a97082331
Llyera <lyer8979@gmail.com>
2026-04-16 17:04:27 +0800

    Update models.py

--- Task1_MedicalAppointmentSystem/models.py
+++ Task1_MedicalAppointmentSystem/models.py
@@ -6,7 +6,7 @@ class Person(ABC):
         self._pid = pid
         self.name = name
         self.phone = phone
-
+        
     def display_info(self) -> str:
         pass
 
@@ -22,7 +22,6 @@ class Patient(Person):
     def display_info(self) -> str:
         return f"Patient [{self._pid}] {self.name} | Phone: {self.phone} | DOB: {self.dob}"
 
-
 class Doctor(Person):
     def __init__(self, pid: str, name: str, phone: str, specialty: str):
         super().__init__(pid, name, phone)
@@ -30,7 +29,6 @@ class Doctor(Person):
 
     def display_info(self) -> str:
         return f"Doctor [{self._pid}] Dr. {self.name} | Specialty: {self.specialty}"
-
 
 class MedicalRecord:
     def __init__(self, date: str, diagnosis: str, prescription: str, notes: str = ""):
@@ -42,7 +40,6 @@ class MedicalRecord:
     def __str__(self):
         return f"{self.date} | Diagnosis: {self.diagnosis} | Prescription: {self.prescription}"
 
-
 class Appointment:
     def __init__(self, app_id: str, patient: Patient, doctor: Doctor, datetime_str: str):
         self.app_id = app_id
@@ -50,7 +47,7 @@ class Appointment:
         self.doctor = doctor
         self.datetime = datetime_str
         self.status = "Booked"
-
+        
     def __str__(self):
         return (f"Appointment[{self.app_id}] {self.datetime} | "
-                f"{self.patient.name} → Dr.{self.doctor.name} ({self.doctor.specialty})")
+                f"{self.patient.name} → Dr.{self.doctor.name} ({self.doctor.specialty}) | Status: {self.status}")
